9cf5459c64757661e8c30999486a8f569f96e8c5
梦夜十六 <mengyeshiliu@gmail.com>
2026-07-30 00:20:24 +0800

    refactor: switch to maven-core npm package

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@maven/ai-core', '@maven/ai-types', '@maven/utils', '@maven/stores'],
+  transpilePackages: ['maven-core'],
 };
 module.exports = nextConfig;
